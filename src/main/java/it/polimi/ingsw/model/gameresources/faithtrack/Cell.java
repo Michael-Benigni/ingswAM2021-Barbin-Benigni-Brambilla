f8f7ff0e6fb11fc6e46005dec600a108fb8fdec7
Gianluca Brambilla <gianluca2.brambilla@mail.polimi.it>
2021-04-09 17:37:32 +0200

    Added the classes Section and Cell. The first one contain a wrong constructor method that will be replaced after the creation of the Faith Track in the database.

---
+++ src/main/java/it/polimi/ingsw/model/gameresources/faithtrack/Cell.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw.model.gameresources.faithtrack;
+
+
+/**
+ * Abstract class that represents a cell of the faith track.
+ */
+public abstract class Cell {
+
+    void activateCell(){}
+}
---
+++ src/main/java/it/polimi/ingsw/model/gameresources/faithtrack/Section.java
@@ -0,0 +1,17 @@
+package it.polimi.ingsw.model.gameresources.faithtrack;
+
+import java.util.ArrayList;
+
+/**
+ * Abstract class that represents a section of the faith truck. A section can be a classic one or a vatican report one.
+ */
+public abstract class Section {
+
+    ArrayList<Cell> listCell;
+
+    //TODO: this constructor method is wrong. It's used only to build correctly the faith track on the json file.
+    public Section(ArrayList<Cell> listCell) {
+        this.listCell = listCell;
+    }
+
+}
