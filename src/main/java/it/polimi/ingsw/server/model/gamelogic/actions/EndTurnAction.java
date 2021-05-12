9ea135a5bcf45b01aa7259f2c3c61eeb27f183fd
Barbin-Alessandro <alessandro.barbin@mail.polimi.it>
2021-05-12 15:04:05 +0200

    corrected the handwriting of exception

--- src/main/java/it/polimi/ingsw/server/model/gamelogic/actions/EndTurnAction.java
+++ src/main/java/it/polimi/ingsw/server/model/gamelogic/actions/EndTurnAction.java
@@ -1,13 +1,15 @@
 package it.polimi.ingsw.server.model.gamelogic.actions;
 
+import it.polimi.ingsw.server.exception.*;
 import it.polimi.ingsw.server.model.gamelogic.Action;
 import it.polimi.ingsw.server.model.gamelogic.Game;
 import it.polimi.ingsw.server.model.gamelogic.Player;
 
-class EndTurnAction implements Action {
+public class EndTurnAction implements Action {
 
     @Override
-    public void perform(Game game, Player player) throws Exception {
+    public void perform(Game game, Player player) throws WrongInitialConfiguration, WrongCellIndexException,
+            CellNotFoundInFaithTrackException, GameOverByFaithTrackException, NegativeVPAmountException {
         new DiscardAllResources().perform(game, player);
         game.getCurrentTurn().terminate(game);
         game.setNextPlayer();
