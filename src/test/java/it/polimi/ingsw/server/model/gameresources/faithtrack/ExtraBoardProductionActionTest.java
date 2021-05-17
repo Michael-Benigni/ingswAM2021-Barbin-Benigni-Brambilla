715582c844167def51bbbb64d414fe7016f0236a
Barbin-Alessandro <alessandro.barbin@mail.polimi.it>
2021-05-17 21:51:23 +0200

    tests improvement

--- src/test/java/it/polimi/ingsw/server/model/gameresources/faithtrack/ExtraBoardProductionActionTest.java
+++ src/test/java/it/polimi/ingsw/server/model/gameresources/faithtrack/ExtraBoardProductionActionTest.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.server.model.gameresources.faithtrack;
 
 import it.polimi.ingsw.server.model.cards.leadercards.LeaderCard;
 import it.polimi.ingsw.server.model.cards.leadercards.Requirement;
+import it.polimi.ingsw.server.model.exception.AlreadyUsedForProuctionException;
 import it.polimi.ingsw.server.model.gamelogic.ActionTest;
 import it.polimi.ingsw.server.model.gamelogic.actions.ExtraBoardProductionAction;
 import it.polimi.ingsw.server.model.gamelogic.actions.StartProductionAction;
@@ -17,11 +18,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ExtraBoardProductionActionTest extends ActionTest {
 
-    /*@Test
+    @Test
     void performTest() throws Exception {
         player1.getPersonalBoard().getStrongbox().store(new StorableResource(ResourceType.COIN, 1));
         FaithMarker faithMarker = game.getGameBoard().getFaithTrack().getMapOfFaithMarkers().get(player1);
-        Cell cellBefore = game.getGameBoard().getFaithTrack().getMapOfFaithMarkers().get(player1).getCurrentCell();
+        Cell cellBefore = faithMarker.getCurrentCell();
         ArrayList<Requirement> requirements = new ArrayList<>();
         VictoryPoint victoryPoints = new VictoryPoint(1);
         LeaderCard leaderCard = new LeaderCard(1, requirements, victoryPoints, null);
@@ -32,9 +33,20 @@ class ExtraBoardProductionActionTest extends ActionTest {
         StartProductionAction startProductionAction = new StartProductionAction();
         startProductionAction.perform(game, player1);
         extraBoardProductionAction.perform(game, player1);
-        assertTrue(cellBefore.equals(game.getGameBoard().getFaithTrack().getMapOfFaithMarkers().get(player1).getCurrentCell()));
-        ArrayList<StorableResource> resources = player1.getPersonalBoard().getStrongbox().getAllResources();
+        assertFalse(cellBefore.equals(game.getGameBoard().getFaithTrack().getMapOfFaithMarkers().get(player1).getCurrentCell()));
+        ArrayList<StorableResource> resources = player1.getPersonalBoard().getTempContainer().getAllResources();
         assertTrue(resources.size() == 1);
         assertTrue(resources.get(0).equals(new StorableResource(ResourceType.STONE, 1)));
-    }*/
+        assertTrue(player1.getPersonalBoard().getStrongbox().getAllResources().isEmpty());
+        player1.getPersonalBoard().getStrongbox().store(new StorableResource(ResourceType.COIN, 1));
+
+        try{
+            extraBoardProductionAction.perform(game, player1);
+            fail();
+        }
+        catch (AlreadyUsedForProuctionException e) {
+            assertTrue(true);
+        }
+
+    }
 }
