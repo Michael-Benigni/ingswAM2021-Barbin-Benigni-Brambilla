c0e83eafa1d7592939de74cff4c5faa7fbd307c3
Barbin-Alessandro <alessandro.barbin@mail.polimi.it>
2021-06-22 11:44:42 +0200

    added some buttons in the GUIWaitingRoomState

--- src/main/java/it/polimi/ingsw/client/view/ui/gui/states/GUIWaitingRoomState.java
+++ src/main/java/it/polimi/ingsw/client/view/ui/gui/states/GUIWaitingRoomState.java
@@ -22,32 +22,64 @@ public class GUIWaitingRoomState extends GUIState{
     }
 
     @Override
-    public void buildScene(GUI gui) {
-            GridPane grid = new GridPane();
-            grid.setAlignment(Pos.CENTER);
-            grid.setHgap(10);
-            grid.setVgap(10);
-            grid.setPadding(new Insets(25, 25, 25, 25));
-            Scene scene = new Scene(grid, 200, 200);
+    public void buildScene(GUI gui){
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+        Scene scene = new Scene(grid, 200, 200);
 
-            Text scenetitle = new Text("Welcome");
-            scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-            grid.add(scenetitle, 0, 0, 2, 1);
+        Text scenetitle = new Text("Welcome");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
 
-            Label userName = new Label("Username:");
-            grid.add(userName, 0, 1);
 
-            TextField userTextField = new TextField();
-            grid.add(userTextField, 1, 1);
+        Label userName = new Label("Username:");
+        grid.add(userName, 0, 1);
 
-            Button button = new Button ("username");
-            button.setOnAction (actionEvent -> {
-                String a = userTextField.getText ();
-                gui.getInterpreter ().addInteraction ("username", a);
-                new MoveService (WaitingRoomMove.SET_USERNAME.getMove (), gui).start ();
-            });
-            grid.add (button, 2,3);
-            setScene (scene);
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+
+        Button button = new Button ("Set your username");
+        button.setOnAction (actionEvent -> {
+            String a = userTextField.getText ();
+            gui.getInterpreter ().addInteraction ("username", a);
+            new MoveService (WaitingRoomMove.SET_USERNAME.getMove (), gui).start ();
+        });
+
+        Button firstFree = new Button ("Automatic matchmaking");
+        firstFree.setOnAction(actionEvent -> {
+            gui.getInterpreter ().addInteraction ("room", "FIRST FREE");
+            new MoveService(WaitingRoomMove.CHOOSE_ROOM.getMove(), gui).start();
+        });
+
+        Button newRoom = new Button ("Create a new game");
+        newRoom.setOnAction(actionEvent -> {
+            gui.getInterpreter ().addInteraction ("room", "NEW");
+            new MoveService(WaitingRoomMove.CHOOSE_ROOM.getMove(), gui).start();
+        });
+
+        Button existentGame = new Button("Join a friend's game");
+        existentGame.setOnAction(actionEvent -> {
+            gui.getInterpreter ().addInteraction ("room", "EXISTENT");
+            new MoveService(WaitingRoomMove.CHOOSE_ROOM.getMove(), gui).start();
+        });
+
+        Label existentMatch = new Label("Insert a match ID to join " +
+                "\nyour friends party! :D");
+        grid.add(existentMatch, 0, 2);
+
+        TextField matchIDTextField = new TextField();
+        grid.add(matchIDTextField, 1, 2);
+
+        grid.add (button, 2,1);
+        grid.add(firstFree, 3, 5);
+        grid.add(newRoom, 3, 6);
+        grid.add(existentGame, 3, 7);
+
+        setScene (scene);
+
+
     }
-
 }
